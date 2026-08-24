4049189f41460ec6f1033c746aa37439d1ff056a
Matteo Heidelberger <matteo.heidelberger@viacesi.fr>
2026-08-24 08:21:00 +0200

    feat(repl): basics of the REPL with the scanner

--- repl.go
+++ repl.go
@@ -1,8 +1,37 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	s := strings.ToLower(text)
 	return strings.Fields(s)
+}
+
+func startREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		output := cleanInput(scanner.Text())
+
+		if len(output) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", output[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
